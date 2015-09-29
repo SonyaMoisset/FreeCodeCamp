81520802251fb60097bbcc7ba9407aad864b13b7
Sonya Moisset <sonya.moisset@gmail.com>
2015-09-29 20:54:15 +0900

    Update 01_Meet_Bonfire.js

--- Basic_Algorithm_Scripting/01_Meet_Bonfire.js
+++ Basic_Algorithm_Scripting/01_Meet_Bonfire.js
@@ -1,4 +1,10 @@
+// 01. Meet Bonfire
+// Your goal is to fix the failing test.
+
 function meetBonfire(argument) {
-  console.log("you can read this function's argument in the developer tools", argument);
-  return true;
+  console.log("You can read this function's argument in the developer tools", argument);
+  
+  return true; // Change the boolean value from false to true
 }
+
+meetBonfire("You can do this!");

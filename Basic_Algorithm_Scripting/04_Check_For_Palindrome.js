4a14a9b3778569a9cd49129da5c6061a1c8b7e08
Sonya Moisset <sonya.moisset@gmail.com>
2015-10-03 17:50:37 +0900

    Update 04_Check_For_Palindrome.js

--- Basic_Algorithm_Scripting/04_Check_For_Palindrome.js
+++ Basic_Algorithm_Scripting/04_Check_For_Palindrome.js
@@ -1,4 +1,23 @@
+// 04. Check for Palindromes
+// Return true if the given string is a palindrome. Otherwise, return false.
+
 function palindrome(str) {
-  str = str.toLowerCase().replace(/[^a-z]+/g,"");
-  return str === str.split("").reverse().join("");
+  return true;
 }
+
+palindrome("eye");
+
+// Solution 1
+
+function palindrome(str) {
+  // Step 1. Use RegExp to remove unwanted characters from the string + lowercase the string
+  var regLowStr = str.replace(/[\W_]/g, '').toLowerCase(); //or var regLowStr = str.replace(/[^A-Za-z0-9_]/g, '').toLowerCase();
+  
+  // Step 2. Use the chaining methods from 02. Reverse a String
+  var reverseStr = regLowStr.split('').reverse().join('');
+  
+  // Step 3. Check if reverseString is strictly equals to lowerString 
+  return reverseStr === regLowStr;
+}
+
+palindrome("eye");

625a6c50e43dce3c1a65fb5524ff982b71cbf822
Sonya Moisset <sonya.moisset@gmail.com>
2015-10-08 21:28:58 +0900

    Update 07_Return_Largest_Numbers_In_Arrays.js

--- Basic_Algorithm_Scripting/07_Return_Largest_Numbers_In_Arrays.js
+++ Basic_Algorithm_Scripting/07_Return_Largest_Numbers_In_Arrays.js
@@ -1,13 +1,35 @@
+// 07. TReturn Largest Numbers in Arrays
+// Return an array consisting of the largest number from each provided sub-array. 
+
 function largestOfFour(arr) {
-  var largestNumber = [0, 0, 0, 0];
+  return arr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// Solution
+
+function largestOfFour(arr) {
+  // Step 1. Create a variable that will host the result of the 4 sub-arrays
+  var largestNumber = [0, 0, 0, 0]; // The provided array will contain exactly 4 sub-arrays
+  var i = 0;
+  var lenI = arr.length;
   
-  for(var i = 0; i < arr.length; i++) {
-    for(var j = 0; j < arr[i].length; j++) {
-      if(arr[i][j] > largestNumber[i] ) {
-        largestNumber[i] = arr[i][j];
+  // Step 2. Create the first FOR loop to iterate through the arrays
+  for(; i < lenI; i++) {
+    var j = 0;
+    var lenJ = arr[i].length;
+  
+  // Step 3. Create the second FOR loop to iterate through the sub-arrays
+    for(; j < lenJ; j++) {
+      if(largestNumber[i] < arr[i][j]) { // In the first iteration arr[0][0], 4 will be the largest number
+      // In [4, 5, 1, 3], if arr[i][j] => arr[0][1] => 5 and largestNumber[i] => 4, so 5 will become the largest number
+        largestNumber[i] = arr[i][j]; // Then largestNumber takes this new value
       }
     }
   }
-  
+  // Step 4. Return the largest numbers of each sub-arrays
   return largestNumber;
 }
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);

343d2b2355ed7c15d3a3b947806337c8b15134b4
Sonya Moisset <sonya.moisset@gmail.com>
2015-10-10 21:41:56 +0900

    Update 09_Repeat_A_String.js

--- Basic_Algorithm_Scripting/09_Repeat_A_String.js
+++ Basic_Algorithm_Scripting/09_Repeat_A_String.js
@@ -1,8 +1,30 @@
+// 08. Repeat a string repeat a string
+// Repeat a given string (first argument) n times (second argument)
+
 function repeat(str, num) {
+  return str;
+}
+
+repeat("abc", 3, "");
+
+// Solution
+
+function repeat(str, num) {
+  // We will use an if/else if/else statement to check each case and the repeat() method to repeat the string
+  // Step 1. If the number equals 0, return the string 
   if (num === 0) {
     return str;
-  } else if (num > 0) {
+  }
+  
+  // Step 2. If the number is positive, return the string n times with the repeat() method
+  else if (num > 0) {
     return str.repeat(num);
-  } else
+  }
+  
+  // Step 3. If the number is negative, return an empty string
+  else {
     return "";
+  }
 }
+
+repeat("abc", 3, "");

175eb85ff1e7bf20edbb5f54b50c8a4bf3b9156f
Sonya Moisset <sonya.moisset@gmail.com>
2015-10-15 22:24:29 +0900

    Update 13. Mutations.js

--- Basic_Algorithm_Scripting/13. Mutations.js
+++ Basic_Algorithm_Scripting/13. Mutations.js
@@ -1,4 +1,4 @@
-// 12. Mutations
+// 13. Mutations
 // Return true if the string in the first element of the array contains all of the letters of the string 
 // in the second element of the array.
 
